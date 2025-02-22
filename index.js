16541edc32aab222f0e25c5733a20388f1a53d90
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-02-22 18:47:55 +0600

    latest post api is loaded here dynamically

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function displayAllPosts(posts){
         <!-- Author & Category -->
         <div class="flex items-center space-x-3">
             <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center">
-                <span class="text-green-500 text-lg font-bold">•</span>
+                <span class="  text-4xl font-bold ${post.isActive?"text-green-500":"text-red-500"}">•</span>
             </div>
             <p class="text-sm text-gray-600">
                 <span class="font-medium">${post.category}</span> · ${post.author.name}
@@ -73,3 +73,60 @@ function addDescription( description,view_count){
 }
 // button clicked 
 
+
+
+
+function latestPostLoader(){
+    fetch(' https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    .then(res=>res.json())
+    .then(data=>displayLatestPost(data))
+}
+
+function displayLatestPost(data){
+    // console.log(data);
+    const latestPostContainer = document.getElementById('latest-post-container');
+    data.forEach(e => {
+        // console.log(element)
+        const div = document.createElement('div');
+        div.innerHTML=`
+            <div class="card lg:w-96 pb-5 bg-base-100 shadow-2xl">
+          <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+              <img
+                  src=${e.cover_image}
+                  alt="Shoes"
+                  class="rounded-xl"
+              />
+          </figure>
+          <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+              <p class="opacity-50 text-start">
+                  <i class="fa-solid fa-calendar-days me-2"></i>${e.author.posted_date ? e.author.posted_date : "No Publish Date"}
+              </p>
+              <h2 class="card-title text-start">${e.title}</h2>
+              <p class="text-start">
+                  ${e.description}
+              </p>
+              <div class="card-actions flex gap-5 items-center">
+                  <div class="avatar">
+                      <div
+                          class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+                      >
+                          <img
+                          src=${e.profile_image}
+                          />
+                      </div>
+                  </div>
+              <div>
+              <h3 class="text-start font-extrabold">${e.author.name}</h3>
+              <p class="text-start opacity-60">${e.author.designation ? e.author.designation : "Unknown "}</p>
+          </div>
+      </div>
+        `
+
+        latestPostContainer.appendChild(div)
+    });
+
+
+}
+
+
+latestPostLoader()
